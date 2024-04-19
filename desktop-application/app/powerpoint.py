04cdea8b8c12dd9edb2516d370eb7cd339c68c54
Makiya Crochiere <crochieremakiya@gmail.com>
2024-04-19 13:57:18 -0500

    all images paths in  graphics folders have their paths mapped to classes

--- desktop-application/app/powerpoint.py
+++ desktop-application/app/powerpoint.py
@@ -7,6 +7,7 @@ imgpath = "./desktop-application/app/graphics/"
 fldrList = ['clustertables', 'dials', 'questiongraphs', 'wordchart', 'wordgraphs', 'tables']
 typList = ['overall', 'department', 'position']
 prs = Presentation("./desktop-application/app/powerpoint/baseLayout.pptx")
+pictures = []
 
 #for slide in prs.slides:
     #print(slide.slide_id)
@@ -22,22 +23,44 @@ class p_slide:
     def __init__(self, title):
         self.title = title
 
+class imageList:
+    def __init__(self, fldr, pics):
+        self.fldr = fldr
+        self.pics = pics
+
 class image:
-    def __init__(self, typ, category):
-        self.type = typ
-        self.category = category
+    def __init__(self, path, name, typ):
+        self.path = path
+        self.name = name
+        self.typ = typ
+
+def initImageList(path, fldr, arr):
+    basepath = path
+    imgs = []
+    for i in arr:
+        fullname = i
+        typ = fullname.split('_')[0]
+        imgs.append(image(basepath+i, fullname, typ))
+
+    imgList  = imageList(fldr, imgs)
+    return imgList
 
 
-def addImages(imgpath, fldrList):
+def initPresImages(imgpath, fldrList, pictures):
     for fldr in fldrList:
         arr = []
         for images in os.listdir(imgpath+fldr+'/'):
             if images.endswith(".png"):
+                #append all image paths
                 arr.append(images)
+
+        #initialize 
+        lst = initImageList((imgpath+fldr+'/'), fldr, arr)
+        pictures.append(lst)
+        
 
 
         print("temp")
 
-
-
-addImages(imgpath, fldrList)
+#once this runs all the image paths are chached 
+initPresImages(imgpath, fldrList, pictures)
